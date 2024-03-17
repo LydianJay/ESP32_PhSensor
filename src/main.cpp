781ac224ef7e710a406d548d74572eb1af377c86
LydianJay <lloydjayedradan@gmail.com>
2024-03-17 19:05:12 +0800

    added firebase

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,73 @@
 #include <Arduino.h>
 #include <Firebase_ESP_Client.h>
+#include <addons/TokenHelper.h>
+#include <addons/RTDBHelper.h>
 #include <WiFiManager.h>
+#include <Adafruit_ADS1X15.h>
 #include "credentials.h"
 FirebaseData fbdo;
-
+Adafruit_ADS1115  ads;     
 FirebaseAuth auth;
 FirebaseConfig config;
 
 
-constexpr uint8_t pinPHSensor = 27;
+
 
 void setup() {
   Serial.begin(115200);
-  pinMode(pinPHSensor, INPUT);
-  /*WiFiManager wm;
-  wm.autoConnect("GSMtracker", "admin123");
+  WiFiManager wm;
+  wm.autoConnect("PH_SENSOR", "admin123");
   
   config.api_key = API_KEY;
   config.database_url = DATABASE_URL;
+  
   Firebase.reconnectNetwork(true);
   fbdo.setResponseSize(2048);
-
+  fbdo.setBSSLBufferSize(4096, 1024);
+  config.token_status_callback = tokenStatusCallback;
+  if (Firebase.signUp(&config, &auth, "", "")){
+    Serial.println("Signup success!");
+    
+  }
+  else{
+    Serial.printf("%s\n", config.signer.signupError.message.c_str());
+  }
   Firebase.begin(&config, &auth);
-  */
+  Firebase.setDoubleDigits(5);
+  Firebase.reconnectNetwork(true);
+  
 
-
+  if(!ads.begin()){
+    Serial.println("Error ADS!");
+  }
+  Serial.print("Firebase status: "); Serial.println(Firebase.ready());
 }
 
 
 
 
 void loop() {
-  uint16_t aRead = analogRead(pinPHSensor);
-  float miliVolt = (float)aRead * (3.3/4095.0);
-  float ph = (miliVolt / 3.3) * 15;
 
-  Serial.print("ADC: "); Serial.println(aRead);
-  Serial.print("miliVolt: "); Serial.println(miliVolt);
-  Serial.print("PH: "); Serial.println(ph);
+  
+  
+  if(Firebase.ready()) {
+    
+    float vcc = ads.computeVolts( ads.readADC_SingleEnded(0));
+    float ph = (15 - ((vcc / 5.0) * 15)) - 0.5;
+
+    Serial.print("vcc: "); Serial.println(vcc);
+    Serial.print("Ph: "); Serial.println(ph);
+    Serial.printf("Set float... %s\n", Firebase.RTDB.setFloat(&fbdo, F("/ph/current"), ph) ? "ok" : fbdo.errorReason().c_str());
+  }
+
+
+  
+
+
 
   //Serial.print("Ph: ");
  // Serial.println(ph);
-  delay(1000);
+ // Serial.println("Waiting...");
+  delay(3100);
 }
 
